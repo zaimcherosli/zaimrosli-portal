00547dca336fff476c17acbddf448674cc08637b
zaimcherosli <huzaimrosli@gmail.com>
2026-08-13 11:13:27 +0800

    fix: slugify undefined error and add 20 Bangi photos to properties-data.js

--- properties-data.js
+++ properties-data.js
@@ -1,4 +1,93 @@
+const bangiPhotos = [
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-01-6a631783b1fff3.43559391.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-02-6a631783b200b1.37824749.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-03-6a631783b200d0.48268283.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-04-6a631783b200f0.99549873.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-05-6a631783b20107.93122438.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-06-6a631783b20117.72403872.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-07-6a631783b20125.75879529.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-08-6a631783b20135.62711527.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-09-6a631783b20140.30902410.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-10-6a631783b20156.15346964.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-11-6a631783b20167.01595962.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-12-6a631783b20186.98519182.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-13-6a631783b20193.77843587.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-14-6a631783b201a2.38777950.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-15-6a631783b201b1.78808807.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-16-6a631783b201c4.28673750.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-17-6a631783b201d4.21597875.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-18-6a631783b201e4.73301592.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-19-6a631783b201f3.48610393.jpg',
+  'https://cdn.propmall.net/media/img-property/list-156678/list-156678-20-6a631783b20201.34096384.jpg'
+];
+
+const bangiDesc = `WTL / FOR RENTAL
+
+🔥 NEWLY PAINTED 🔥
+🔥 EXTENDED UNIT 🔥
+‼️ MOVE IN CONDITION ‼️
+
+Partially Furnished Double Storey Semi-D House Taman Kajang Impian @ Bandar Baru Bangi, Bangi, Selangor
+
+DETAILS:
+================
+- Type: Semi-Detached House
+- Built-up size: 2700 square feet
+- Land size: 3000 square feet
+- 4 bedrooms
+- 4 bathrooms
+- Prefer Malay & Muslim Tenant
+- Newly Painted`;
+
 const PROPERTIES_DATA = [
+  {
+    "id": "prop-156678",
+    "slug": "partially-furnished-double-storey-semi-d-house-taman-kajang-impian-bandar-baru-bangi-bangi-selangor",
+    "title": "Partially Furnished Double Storey Semi-D House Taman Kajang Impian @ Bandar Baru Bangi, Bangi, Selangor",
+    "price": 2500,
+    "priceStr": "RM 2,500 / bln",
+    "location": "Taman Kajang Impian, Bandar Baru Bangi, Selangor",
+    "region": "Selangor",
+    "type": "Semi-D",
+    "status": "rent",
+    "category": "Rental",
+    "beds": 4,
+    "baths": 4,
+    "parking": 2,
+    "size": 2700,
+    "landSize": "3,000 sqft",
+    "tenure": "-",
+    "lotType": "-",
+    "refUrl": "https://propmall.co/share/egd0t9u5nw",
+    "image": bangiPhotos[0],
+    "images": bangiPhotos,
+    "description": bangiDesc
+  },
+  {
+    "id": "prop-163803",
+    "slug": "for-rent-available-in-july-2026-townhouse-kita-bayu-cybersouth",
+    "title": "FOR RENT AVAILABLE IN JULY 2026 TOWNHOUSE KITA BAYU @ CYBERSOUTH",
+    "price": 1500,
+    "priceStr": "RM 1,500 / bln",
+    "location": "Kita Bayu, Cybersouth, Sepang, Selangor",
+    "region": "Selangor",
+    "type": "Townhouse",
+    "status": "rent",
+    "category": "Rental",
+    "beds": 4,
+    "baths": 2,
+    "parking": 2,
+    "size": 920,
+    "landSize": "22 x 75",
+    "tenure": "-",
+    "lotType": "-",
+    "refUrl": "https://propmall.my/share/efkrhdx8pn",
+    "image": "https://cdn.propmall.net/media/img-property/list-163803/list-163803-01-6a6c6010273cb4.94800944.jpg",
+    "images": [
+      "https://cdn.propmall.net/media/img-property/list-163803/list-163803-01-6a6c6010273cb4.94800944.jpg"
+    ],
+    "description": "FOR RENT AVAILABLE IN JULY 2026 TOWNHOUSE KITA BAYU @ CYBERSOUTH"
+  },
   {
     "id": "prop-146291",
     "slug": "factory-with-2-storey-office-taman-industri-sijangkang-utama-teluk-panglima-garang",
@@ -9,19 +98,20 @@ const PROPERTIES_DATA = [
     "region": "Selangor",
     "type": "Kilang",
     "status": "rent",
+    "category": "Rental",
     "beds": 0,
     "baths": 4,
     "parking": 10,
     "size": 50407,
     "landSize": "2.1157 Acres",
-    "tenure": "Freehold",
-    "lotType": "Non-Bumi Lot",
+    "tenure": "-",
+    "lotType": "-",
     "refUrl": "https://propmall.co/share/eezmfo1hta",
     "image": "https://cdn.propmall.net/media/img-property/list-146291/list-146291-01-69eb366f39d399.50503260.jpg",
     "images": [
       "https://cdn.propmall.net/media/img-property/list-146291/list-146291-01-69eb366f39d399.50503260.jpg"
     ],
-    "description": "Factory With Office In Taman Industri Sijangkang Utama, Teluk Panglima Garang For Rent:\n\nFactory/Warehouse Information:\n\nLocation / address : Taman Industri Sijangkang Utama, 42500 Telok Panglima Garang, Daerah Kuala Langat\n\nLand Area : 2.1157 Acres\n\nBuilt Up : 4,683 m2 / 50,407 sqft\n\nDimension : 174.87m x 25.45m / 574 ft x 83.5 ft\n\nOffice Area : 478 m2 / 5,145 sqft\n\nFactory Area: 4,205 m2 / 45,262 sqft\n\nRear Road : 30 ft wide\n\nOpen Space for Front : 50 ft\n\nHeight : 12.2 m / 40 ft\n\nFloor Loading : 4 ton/m2\n\nPower Supply : 1000 amp\n\nLoading Dock : No\n\nFactory Category : Light Industrial\n\nCF/CCC : Obtained\n\nAutomatic Sprinkler System : Yes\n\nNumber of MS. Sliding Door : 8 Nos\n\nCurrent Status: Tenanted\n\nFeatures & Access:\n\u2022 Well connected to Port Klang\n\u2022 Well kept factory\n\u2022 Easy access to Shah Alam Highway (KESAS) and SKVE"
+    "description": "Factory With Office In Taman Industri Sijangkang Utama, Teluk Panglima Garang For Rent:\n\nFactory/Warehouse Information:\n\nLocation / address : Taman Industri Sijangkang Utama, 42500 Telok Panglima Garang, Daerah Kuala Langat\n\nLand Area : 2.1157 Acres\n\nBuilt Up : 4,683 m2 / 50,407 sqft\n\nDimension : 174.87m x 25.45m / 574 ft x 83.5 ft\n\nOffice Area : 478 m2 / 5,145 sqft\n\nFactory Area: 4,205 m2 / 45,262 sqft\n\nRear Road : 30 ft wide\n\nOpen Space for Front : 50 ft\n\nHeight : 12.2 m / 40 ft\n\nFloor Loading : 4 ton/m2\n\nPower Supply : 1000 amp\n\nLoading Dock : No\n\nFactory Category : Light Industrial\n\nCF/CCC : Obtained\n\nAutomatic Sprinkler System : Yes\n\nNumber of MS. Sliding Door : 8 Nos\n\nCurrent Status: Tenanted\n\nFeatures & Access:\n• Well connected to Port Klang\n• Well kept factory\n• Easy access to Shah Alam Highway (KESAS) and SKVE"
   }
 ];
 
