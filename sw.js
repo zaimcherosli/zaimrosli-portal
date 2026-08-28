f3d9e5e507bbfb720bf1b8bab45ff8b80965198c
Zaim Rosli <huzaimrosli@gmail.com>
2026-08-28 12:22:07 +0800

    feat(calculator): translate all calculator contents and sharing text to professional English

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zaimrosli-pwa-v98';
+const CACHE_NAME = 'zaimrosli-pwa-v99';
 
 // Install Event — Skip waiting immediately so new SW activates fast
 self.addEventListener('install', (event) => {
